d92c919640ce726c4ea196dc424f87d7017cd56e
Jesus1443 <jesusrm15gg@outlook.com>
2026-02-26 10:39:32 -0600

    Practica 12 Async y await

---
+++ 12_Async_await.dart
@@ -0,0 +1,21 @@
+void main() async {
+  print('Inicio del programa');
+  
+  try {
+    final value = await httpGet('http://api.jesus.dev');
+    print(value);
+  }catch(err) {
+    print('Error: $err');
+  }
+  
+  print('Final del programa');
+}
+
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 2));
+  
+  throw 'Error en la petición';
+    
+  //return 'Tenemos un valor!!';
+  }
+ 
