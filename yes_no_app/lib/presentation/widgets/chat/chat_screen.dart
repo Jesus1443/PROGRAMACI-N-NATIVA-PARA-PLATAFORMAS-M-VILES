50ac90218df5b9a9fe4c768872c06e2c529426e6
JesusR <jesusrm15gg@outlook.com>
2026-03-25 11:31:53 -0600

    Actualizo funcionalidad en yes_no_app

---
+++ yes_no_app/lib/presentation/widgets/chat/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(padding: const EdgeInsets.all(4.0), child: CircleAvatar(backgroundImage: NetworkImage('https://content.emarket.pe/common/collections/standard/e6/c7/e6c7d2be-59e7-481c-aa7a-096185770e8e.jpg'),)),
+        title: const Text('CHAT SCREEN'),
+      ),
+      body: _Chatview(),
+    );
+  }
+}
+
+class _Chatview extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return (index % 2 == 0)
+                  ? const MyMessageBubble()
+                  :
+                  const HerMessageBubble();
+                },
+              )
+                
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          //height: 10,
+          width: 300,
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: BorderRadius.circular(20),
+            ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0
+            ),
+            child: Text('Hola, como estas?',
+            style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 10,),
+        ],
+    );
+  }
+}
